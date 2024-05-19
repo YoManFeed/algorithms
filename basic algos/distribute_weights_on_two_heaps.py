fe2f4673a14464241883057a2686aea2568d9d34
popit <mlgpavlin231@gmail.com>
2024-05-19 10:48:28 +0300

    исправил ошибку и добавил тесты

--- basic algos/distribute_weights_on_two_heaps.py
+++ basic algos/distribute_weights_on_two_heaps.py
@@ -1,64 +1,78 @@
-# W, n = map(int, input().split())
-# a = [0] + list(map(int, input().split()))
-
-# W, N = 10, 4
-# a = [0, 1, 2, 3, 4]
-
-W, N = 10, 3
-a = [0, 2, 3, 5]
-
-W, N = 14, 4
-a = [0, 1, 1, 3, 4]
-
-W = W // 2
-
-dp = [[[0] * (W + 1) for _ in range(W + 1)] for _ in range(N + 1)]
-for i in range(N+1):
-    dp[i][0][0] = 1
+from collections import Counter
 
 
-prev1 = [['.'] * (W+1) for _ in range(W+1)]
+def build_two_servers(gpus: tuple) -> tuple:
+    W = sum(gpus) // 2
+    N = len(gpus)
+    a = [0] + sorted(list(gpus))
 
-max_value = 0
-for n in range(N+1):
-    for i in range(0, W+1):
-        for j in range(0, W+1):
-            dp[n][i][j] = dp[n-1][i][j]
-            w = a[n]
-            if w <= j and dp[n-1][i][j-w]:
-                dp[n][i][j] = 1
-                if prev1[i][j] == '.':
-                    prev1[i][j] = w
-            if w <= i and dp[n-1][i-w][j]:
-                dp[n][i][j] = 1
-                if prev1[i][j] == '.':
-                    prev1[i][j] = w
-            if i == j and dp[n][i][j]:
-                max_value = max(max_value, i)
+    if N == 0:
+        return ((), (), ())
 
-for plain in dp:
-    for row in plain:
-        print(*row)
-    print()
+    dp = [[[0] * (W + 1) for _ in range(W + 1)] for _ in range(N + 1)]
+    for i in range(N+1):
+        dp[i][0][0] = 1
 
-print('~'*10)
+    prev1 = [['.'] * (W+1) for _ in range(W+1)]
 
-for plain in prev1:
-    print(*plain)
-print(max_value)
+    max_value = 0
+    for n in range(N+1):
+        for i in range(0, W+1):
+            for j in range(0, W+1):
+                dp[n][i][j] = dp[n-1][i][j]
+                w = a[n]
+                if w <= j and dp[n-1][i][j-w]:
+                    dp[n][i][j] = 1
+                    if prev1[i][j] == '.':
+                        prev1[i][j] = w
+                if w <= i and dp[n-1][i-w][j]:
+                    dp[n][i][j] = 1
+                    if prev1[i][j] == '.':
+                        prev1[i][j] = w
+                if i == j and dp[n][i][j]:
+                    max_value = max(max_value, i)
 
-i, j = max_value, max_value
-ans1 = []
-ans2 = []
-while prev1[i][j] > 0:
-    while i > 0:
-        ans1.append(w)
-        i -= w
-        w = prev1[i][j]
+    # for plain in dp:
+    #     for row in plain:
+    #         print(*row)
+    #     print()
+    #
+    # print('~' * 10)
+    #
+    # for plain in prev1:
+    #     print(*plain)
+    # print(max_value)
 
-    while j > 0:
-        ans2.append(w)
-        j -= w
-        w = prev1[i][j]
+    i, j = max_value, max_value
+    serv_1 = []
+    serv_2 = []
+    while prev1[i][j] != 0:
+        while i > 0:
+            serv_1.append(w)
+            i -= w
+            w = prev1[i][j]
+            while j > 0:
+                serv_2.append(w)
+                j -= w
+                w = prev1[i][j]
 
-print(ans1, ans2)
+
+    unused = tuple((Counter(a) - Counter(serv_1) - Counter(serv_2) - Counter([0])).elements())
+
+    return (tuple(serv_1), tuple(serv_2), unused)
+
+
+if __name__ == '__main__':
+    various_gpus = (
+        (1, 1),  # GPUs: (1, 1), server_0: (1), server_1: (1), unused: ()
+        (1, 1, 2, 1),  # GPUs: (1, 1, 2, 1), server_0: (1, 1), server_1: (2), unused: (1)
+        (3, 1),  # GPUs: (3, 1), server_0: (), server_1: (), unused: (1, 3)
+        (),  # GPUs: (), server_0: (), server_1: (), unused: ()
+        (2, 2, 5, 6, 7, 3),  # ...
+        (78, 35, 34, 1, 2, 3, 1, 2, 1, 1)
+    )
+
+    for gpus in various_gpus:
+        result = build_two_servers(gpus)
+        print(f"GPUs: {gpus}, server_0: {result[0]}, server_0: {result[1]}, unused: {result[2]}")
+        assert sum(result[0]) == sum(result[1])
